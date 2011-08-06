bea28ec26d0e59411f7faac0472ce565e254c594
Michelangelo <dragonbe+github@gmail.com>
2011-08-06 23:13:20 +0200

    Running a more focussed test service on a local WSDL file for when the external service is non-responsive

--- tests/library/My/Service/ViesTest.php
+++ tests/library/My/Service/ViesTest.php
@@ -1,44 +1,6 @@
 <?php
 class My_Service_ViestTest extends PHPUnit_Framework_TestCase
 {
-    protected function _createMockResponse($response)
-    {
-        $stub = $this->getMock('My_Service_Vies');
-        $stub->expects($this->any())
-             ->method('validateVat')
-             ->will($this->returnValue($response));
-        return $stub;
-    }
-    public function testGoodValidationOfVatNumber()
-    {
-        $response = new StdClass();
-        $response->countryCode = 'NL';
-        $response->vatNumber = '0123-456-789';
-        $response->requestDate = '1983-06-24';
-        $response->valid = true;
-        $response->name = '';
-        $response->address = '';
-        
-//        $vies = $this->_createMockResponse($response);
-//        
-//        $actual = $vies->validateVat('NL', '0123-456-789');
-//        $this->assertEquals($response, $actual);
-//        $this->assertTrue($actual->valid);
-    }
-    public function testBadValidationOfVatNumber()
-    {
-        $response = new StdClass();
-        $response->countryCode = 'BE';
-        $response->vatNumber = '9876-ABC-321';
-        $response->requestDate = '1983-06-24';
-        $response->valid = false;
-        
-//        $vies = $this->_createMockResponse($response);
-//        
-//        $actual = $vies->validateVat('BE', '9876-ABC-321');
-//        $this->assertEquals($response, $actual);
-//        $this->assertFalse($actual->valid);
-    }
     public function vatNumberProvider()
     {
         return array (
@@ -59,7 +21,7 @@ class My_Service_ViestTest extends PHPUnit_Framework_TestCase
     protected function _createdStubbedViesClient($response)
     {
         $stub = $this->getMockFromWsdl(
-            My_Service_Vies::VIES_WSDL);
+            dirname(__FILE__) . '/_files/checkVatService.wsdl');
         $stub->expects($this->any())
              ->method('checkVat')
              ->will($this->returnValue($response));
