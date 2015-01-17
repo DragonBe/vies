b0bdd4f75300f0d58491d2da426783f03a9b67b1
Michelangelo <dragonbe+github@gmail.com>
2015-01-17 14:54:42 +0100

    [SensioLabsInsight issue #17] - Refrain from using exit or die and fixing a newline for the .gitignore file: https://insight.sensiolabs.com/projects/21b019ce-dd1d-4d16-8b74-880b9ee5e795/analyses/17

--- examples/checkVat.php
+++ examples/checkVat.php
@@ -3,16 +3,16 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $vies = new \DragonBe\Vies\Vies();
 if (false === $vies->getHeartBeat()->isAlive()) {
+
     echo 'Service is not available at the moment, please try again later.' . PHP_EOL;
-    exit(1);
+
+} else {
+
+//  Using my own VAT to verify, should be valid
+    $result = $vies->validateVat('BE', '0811231190');
+    echo ($result->isValid() ? 'VALID' : 'INVALID') . ' VAT number' . PHP_EOL;
+
+//  Using bogus VAT to verify, should be invalid
+    $result = $vies->validateVat('BE', '1234567890');
+    echo ($result->isValid() ? 'VALID' : 'INVALID') . ' VAT number' . PHP_EOL;
 }
-
-// Using my own VAT to verify, should be valid
-$result = $vies->validateVat('BE', '0811231190');
-echo ($result->isValid() ? 'VALID' : 'INVALID') . ' VAT number' . PHP_EOL;
-
-// Using bogus VAT to verify, should be invalid
-$result = $vies->validateVat('BE', '1234567890');
-echo ($result->isValid() ? 'VALID' : 'INVALID') . ' VAT number' . PHP_EOL;
-
-exit(0);
