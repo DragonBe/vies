9709d2fcaf2017ecdbe6d0655bfc933f4af0a085
James Blackwell <jwblackwell@gmail.com>
2015-03-25 14:46:49 +0000

    Updating country total to reflect addition of Croatia

--- src/Vies/Vies.php
+++ src/Vies/Vies.php
@@ -27,7 +27,7 @@ class Vies
     const VIES_PROTO = 'http';
     const VIES_DOMAIN = 'ec.europa.eu';
     const VIES_WSDL = '/taxation_customs/vies/checkVatService.wsdl';
-    const VIES_EU_COUNTRY_TOTAL = 27;
+    const VIES_EU_COUNTRY_TOTAL = 28;
 
     /**
      * @var \SoapClient
