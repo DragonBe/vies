c0829c607783eed380484b89d13b9e326567f84f
Jonathan Maron <jonathan.maron@theimagingsource.com>
2015-07-08 08:31:00 +0200

    Added new examples

---
+++ examples/checkVatBatch.php
@@ -0,0 +1,71 @@
+<?php
+
+use DragonBe\Vies\Vies;
+use DragonBe\Vies\ViesException;
+use DragonBe\Vies\ViesServiceException;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$vies = new Vies();
+
+if (false === $vies->getHeartBeat()->isAlive()) {
+
+    echo 'Service is not available at the moment, please try again later.' . PHP_EOL;
+
+} else {
+
+    $splQueue = new SplQueue();
+
+    $vatins = vatinProvider();
+
+    foreach ($vatins as $vatin) {
+        $splQueue->enqueue($vatin);
+    }
+
+    unset($vatins);
+
+    foreach ($splQueue as $item) {
+
+        $vatin = $splQueue->dequeue();
+
+        echo 'Validating ' . $vatin . '... ';
+
+        $countryCode = substr($vatin, 0, 2);
+        $vatNumber   = substr($vatin, 2);
+        $vatNumber   = $vies->filterVat($vatNumber);
+
+        try {
+            $result = $vies->validateVat($countryCode, $vatNumber);     // Validation routine worked as expected.
+            echo ($result->isValid()) ? 'VALID' : 'INVALID';
+        } catch (ViesServiceException $e) {                             // Recoverable exception. Add VATIN back to stack.
+            echo $e->getMessage();                                      // There is probabily a temporary problem with backend VIES service.
+            $splQueue->enqueue($vatin);
+        } catch (ViesException $e) {                                    // Unrecoverable exception.
+            echo $e->getMessage();                                      // Invalid country code etc.
+        }
+
+        echo PHP_EOL;
+    }
+}
+
+
+/**
+ * Load an array of VATINs (country code + VAT number)
+ *
+ * Source:
+ * http://www.braemoor.co.uk/software/vattest.php
+ *
+ * @return array
+ */
+function vatinProvider()
+{
+    $filename = realpath(__DIR__ . '/vatins.php');
+
+    if (is_readable($filename)) {
+        $ret = include $filename;
+    } else {
+        $ret = [];
+    }
+
+    return $ret;
+}
