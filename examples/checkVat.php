35930ab000efdb50027d6001ed75ed8e54e0525d
Jonathan Maron <jonathan.maron@theimagingsource.com>
2015-07-08 11:52:25 +0200

    Updated "checkVat.php" and README.md to reflect the new ViesServiceException.
    Fixed a few little typos.

--- examples/checkVat.php
+++ examples/checkVat.php
@@ -1,6 +1,8 @@
 <?php
 
 use DragonBe\Vies\Vies;
+use DragonBe\Vies\ViesException;
+use DragonBe\Vies\ViesServiceException;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
@@ -12,12 +14,38 @@ if (false === $vies->getHeartBeat()->isAlive()) {
 
 } else {
 
-    // Using my own VAT to verify, should be valid
-    $result = $vies->validateVat('BE', '0811231190');
-    echo ($result->isValid() ? 'VALID' : 'INVALID') . ' VAT number' . PHP_EOL;
+    $vatNumberProvider = [
+        [
+            'BE' => '0811231190',       // valid
+            'HR' => '20649144807'
+        ],
+        [
+            'BE' => '1234567890',       // invalid
+            'ES' => '9999999999',
+        ],
+        [
+            'AA' => '1234567890',       // invalid country code
+            'NO' => '1234567890'
+        ],
+    ];
 
-    // Using bogus VAT to verify, should be invalid
-    $result = $vies->validateVat('BE', '1234567890');
-    echo ($result->isValid() ? 'VALID' : 'INVALID') . ' VAT number' . PHP_EOL;
+    foreach ($vatNumberProvider as $vatNumbers) {
 
+        foreach ($vatNumbers as $countryCode => $vatNumber) {
+
+            echo 'Validating ' . $countryCode . $vatNumber . '... ';
+
+            try {
+                $result = $vies->validateVat($countryCode, $vatNumber); // - Validation routine worked as expected.
+                echo ($result->isValid()) ? 'Valid' : 'Invalid';        //
+            } catch (ViesServiceException $e) {                         // - Recoverable exception.
+                echo $e->getMessage();                                  //   There is probably a temporary problem with back-end VIES service.
+            } catch (ViesException $e) {                                // - Unrecoverable exception.
+                echo $e->getMessage();                                  //   Invalid country code etc.
+            }
+
+            echo PHP_EOL;
+
+        }
+    }
 }
