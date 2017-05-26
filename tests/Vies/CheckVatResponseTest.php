bbd9557fc1cfb733971a7e03639b328bc9c128b1
Michelangelo van Dam <dragonbe+github@gmail.com>
2017-05-26 19:38:11 -0400

    Updating unit tests for 100% code coverage
    Making sure we have 100% code coverage before altering the type hints in the code

--- tests/Vies/CheckVatResponseTest.php
+++ tests/Vies/CheckVatResponseTest.php
@@ -6,6 +6,11 @@ namespace DragonBe\Test\Vies;
 use DragonBe\Vies\CheckVatResponse;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class CheckVatResponseTest
+ *
+ * @package DragonBe\Test\Vies
+ */
 class CheckVatResponseTest extends TestCase
 {
     /**
@@ -50,7 +55,25 @@ class CheckVatResponseTest extends TestCase
     }
 
     /**
+     * Test that a VAT response can be created
+     *
      * @dataProvider validationProvider
+     * @covers \DragonBe\Vies\CheckVatResponse::__construct
+     * @covers \DragonBe\Vies\CheckVatResponse::populate
+     * @covers \DragonBe\Vies\CheckVatResponse::setCountryCode
+     * @covers \DragonBe\Vies\CheckVatResponse::getCountryCode
+     * @covers \DragonBe\Vies\CheckVatResponse::setVatNumber
+     * @covers \DragonBe\Vies\CheckVatResponse::getVatNumber
+     * @covers \DragonBe\Vies\CheckVatResponse::setRequestDate
+     * @covers \DragonBe\Vies\CheckVatResponse::getRequestDate
+     * @covers \DragonBe\Vies\CheckVatResponse::setValid
+     * @covers \DragonBe\Vies\CheckVatResponse::isValid
+     * @covers \DragonBe\Vies\CheckVatResponse::setName
+     * @covers \DragonBe\Vies\CheckVatResponse::getName
+     * @covers \DragonBe\Vies\CheckVatResponse::setAddress
+     * @covers \DragonBe\Vies\CheckVatResponse::getAddress
+     * @covers \DragonBe\Vies\CheckVatResponse::setIdentifier
+     * @covers \DragonBe\Vies\CheckVatResponse::getIdentifier
      */
     public function testCanCreateResponseAtConstruct($validCheck)
     {
@@ -67,6 +90,22 @@ class CheckVatResponseTest extends TestCase
 
     /**
      * @dataProvider validationProvider
+     * @covers \DragonBe\Vies\CheckVatResponse::__construct
+     * @covers \DragonBe\Vies\CheckVatResponse::populate
+     * @covers \DragonBe\Vies\CheckVatResponse::setCountryCode
+     * @covers \DragonBe\Vies\CheckVatResponse::getCountryCode
+     * @covers \DragonBe\Vies\CheckVatResponse::setVatNumber
+     * @covers \DragonBe\Vies\CheckVatResponse::getVatNumber
+     * @covers \DragonBe\Vies\CheckVatResponse::setRequestDate
+     * @covers \DragonBe\Vies\CheckVatResponse::getRequestDate
+     * @covers \DragonBe\Vies\CheckVatResponse::setValid
+     * @covers \DragonBe\Vies\CheckVatResponse::isValid
+     * @covers \DragonBe\Vies\CheckVatResponse::setName
+     * @covers \DragonBe\Vies\CheckVatResponse::getName
+     * @covers \DragonBe\Vies\CheckVatResponse::setAddress
+     * @covers \DragonBe\Vies\CheckVatResponse::getAddress
+     * @covers \DragonBe\Vies\CheckVatResponse::setIdentifier
+     * @covers \DragonBe\Vies\CheckVatResponse::getIdentifier
      */
     public function testCanCreateResponseWithoutNameAndAddressAtConstruct($validCheck)
     {
@@ -84,6 +123,22 @@ class CheckVatResponseTest extends TestCase
 
     /**
      * @dataProvider validationProvider
+     * @covers \DragonBe\Vies\CheckVatResponse::__construct
+     * @covers \DragonBe\Vies\CheckVatResponse::populate
+     * @covers \DragonBe\Vies\CheckVatResponse::setCountryCode
+     * @covers \DragonBe\Vies\CheckVatResponse::getCountryCode
+     * @covers \DragonBe\Vies\CheckVatResponse::setVatNumber
+     * @covers \DragonBe\Vies\CheckVatResponse::getVatNumber
+     * @covers \DragonBe\Vies\CheckVatResponse::setRequestDate
+     * @covers \DragonBe\Vies\CheckVatResponse::getRequestDate
+     * @covers \DragonBe\Vies\CheckVatResponse::setValid
+     * @covers \DragonBe\Vies\CheckVatResponse::isValid
+     * @covers \DragonBe\Vies\CheckVatResponse::setName
+     * @covers \DragonBe\Vies\CheckVatResponse::getName
+     * @covers \DragonBe\Vies\CheckVatResponse::setAddress
+     * @covers \DragonBe\Vies\CheckVatResponse::getAddress
+     * @covers \DragonBe\Vies\CheckVatResponse::setIdentifier
+     * @covers \DragonBe\Vies\CheckVatResponse::getIdentifier
      */
     public function testCanCreateResponseWithArrayAtConstruct($validCheck)
     {
@@ -98,10 +153,72 @@ class CheckVatResponseTest extends TestCase
         $this->assertSame($response['requestIdentifier'], $checkVatResponse->getIdentifier());
     }
 
+    /**
+     * @covers \DragonBe\Vies\CheckVatResponse::__construct
+     * @covers \DragonBe\Vies\CheckVatResponse::getRequestDate
+     */
     public function testDefaultDateIsNow()
     {
         $vatResponse = new CheckVatResponse();
         $this->assertInstanceOf('\\DateTime', $vatResponse->getRequestDate());
         $this->assertSame(date('Y-m-dP'), $vatResponse->getRequestDate()->format('Y-m-dP'));
+    }
+
+    /**
+     * @covers \DragonBe\Vies\CheckVatResponse::__construct
+     * @covers \DragonBe\Vies\CheckVatResponse::populate
+     * @expectedException \InvalidArgumentException
+     */
+    public function testExceptionIsThrownWhenRequiredParametersAreMissing()
+    {
+        $vatResponse = new CheckVatResponse([]);
+    }
+
+    public function requiredDataProvider()
+    {
+        return [
+            ['DE', '123456749', date('Y-m-dP'), true],
+            ['ES', '987654321', date('Y-m-dP'), false],
+        ];
+    }
+
+    /**
+     * @dataProvider requiredDataProvider
+     * @covers \DragonBe\Vies\CheckVatResponse::__construct
+     * @covers \DragonBe\Vies\CheckVatResponse::populate
+     * @covers \DragonBe\Vies\CheckVatResponse::setCountryCode
+     * @covers \DragonBe\Vies\CheckVatResponse::getCountryCode
+     * @covers \DragonBe\Vies\CheckVatResponse::setVatNumber
+     * @covers \DragonBe\Vies\CheckVatResponse::getVatNumber
+     * @covers \DragonBe\Vies\CheckVatResponse::setRequestDate
+     * @covers \DragonBe\Vies\CheckVatResponse::getRequestDate
+     * @covers \DragonBe\Vies\CheckVatResponse::setValid
+     * @covers \DragonBe\Vies\CheckVatResponse::isValid
+     * @covers \DragonBe\Vies\CheckVatResponse::setName
+     * @covers \DragonBe\Vies\CheckVatResponse::getName
+     * @covers \DragonBe\Vies\CheckVatResponse::setAddress
+     * @covers \DragonBe\Vies\CheckVatResponse::getAddress
+     * @covers \DragonBe\Vies\CheckVatResponse::setIdentifier
+     * @covers \DragonBe\Vies\CheckVatResponse::getIdentifier
+     */
+    public function testResponseContainsEmptyValuesWithOnlyRequiredArguments($countryCode, $vatNumber, $requestDate, $valid)
+    {
+        $expectedResult = [
+            'countryCode' => $countryCode,
+            'vatNumber' => $vatNumber,
+            'requestDate' => substr($requestDate, 0, -6),
+            'valid' => $valid,
+            'name' => '---',
+            'address' => '---',
+            'identifier' => '',
+        ];
+        $vatResponse = new CheckVatResponse([
+            'countryCode' => $countryCode,
+            'vatNumber' => $vatNumber,
+            'requestDate' => $requestDate,
+            'valid' => $valid,
+        ]);
+
+        $this->assertSame($expectedResult, $vatResponse->toArray());
     }
 }
