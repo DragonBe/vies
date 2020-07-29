270e618f716ed3447c2beada7ff89f969bac095d
Michelangelo van Dam <dragonbe+github@gmail.com>
2020-07-29 15:35:06 +0200

    Feature 110 - Cli-client : code review fixes
    Many thanks to @peter279k for pointing out this ugly mistake I made.

--- tests/Vies/ViesTest.php
+++ tests/Vies/ViesTest.php
@@ -705,7 +705,7 @@ class ViesTest extends TestCase
         $countryCode = 'BE';
         $vatNumber = '1234567890';
         $resultSet = (new Vies())->splitVatId($vatId);
-        $this->assertSame($resultSet['country'], $countryCode);
-        $this->assertSame($resultSet['id'], $vatNumber);
+        $this->assertSame($countryCode, $resultSet['country']);
+        $this->assertSame($vatNumber, $resultSet['id']);
     }
 }
